e070cee2af4d5696af359c75818eb95eb1e1bf9d
AyujPanchal <b19099@students.iitmandi.ac.in>
2021-04-10 18:11:09 +0530

    Implement Chained Route Handlers

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
+app.route("/articles")
+
+.get(function(req, res){
     Article.find(function(err, foundArticles){
         if(!err){
             res.send(foundArticles);
@@ -32,11 +34,9 @@ app.get("/articles", function(req, res){
         }
         
     });
-});
+})
 
-app.post("/articles", function(req,res){
-    console.log(req.body.title);
-    console.log(req.body.content);
+.post(function(req,res){
 
     const newArticle = new Article({
         title: req.body.title,
@@ -50,17 +50,19 @@ app.post("/articles", function(req,res){
             res.send(err);
         }
     });
-});
+})
 
-app.delete("/articles", function(req, res){
+.delete(function(req, res){
     Article.deleteMany(function(err){
         if(!err){
             res.send("Successfully deleted all articles.");
         } else {
             res.send(err);
         }
-    })
+    });
 });
+
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
